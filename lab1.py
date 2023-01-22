65ae75f57fdf16832bd654342cbb5262601f9a15
ALtyn119 <a_davlatova@kbtu.kz>
2023-01-22 22:17:40 +0600

    #lab1

---
+++ lab1.py
@@ -0,0 +1,151 @@
+
+print("Hello world")
+
+if 5 > 3:
+    print("OK")
+# This is a comment
+
+"""
+and we can write like that
+"""
+Altyn = "clever"
+
+x = 14
+y = 30
+print(x + y)
+
+x = 2
+y = 7
+m = x + y
+print (m)
+myname_is = "Altyn"
+
+q = 'Hello'
+w = 'world'
+print(q, w)
+a = s = d = "Yellow"
+
+e = "mouse"
+def mnl():
+    print("A tiny " + e)
+mnl()
+
+
+#data type
+r = 21
+t = 12.34
+u = "asd"
+print(type(r))
+print(type(t))
+print(type(u))
+
+i = 5.556
+i = int(i)
+print(i)
+
+# casting
+o = int(12.3)
+p = int(85.12)
+print(o)
+print(p)
+
+# string
+a = """ ASDFGHJ cvbnkj cvbnm dtyuknbvg ycgdkhbckjdn  kcvksd hfbdakcbkjs kjdcbskd kjddcksj lksvksln vjbsjv hkgcsdhcj """
+print(a)
+
+s = "bvncmkdldknk"
+print(s[4])
+
+for d in ' atikanmn':
+    print(d)
+
+d = "atikanmn"
+print(len(d))
+
+
+adf = 'Altyn is beautiful'
+print('is' in adf)
+
+
+adf = 'Altyn is beautiful'
+if 'Altyn' in adf:
+    print('Yes')
+
+f = 'Atikanmn'
+print(f[2:7])
+
+f = "assdfg"
+print(f.upper())
+
+g = "   sdfghj"
+print(g.strip())
+print(g.replace('s', "h"))
+
+age = 17
+azx = "My name is Altyn, and I am {}"
+print(azx.format(age))
+
+# pyton boolean
+print( 10 > 9)
+print(109 == 108)
+print(11 < 2)
+
+
+a = 100
+b = 22
+if a > b:
+    print('correct')
+else:
+    print('not correct')
+
+print(bool('assd'))
+print(bool(123))
+print(bool(['asdf', 'asd', 'vbn']))
+print(bool(0))
+print(bool({ }))
+print(bool(()))
+print(bool([]))
+
+def allll():
+    return True
+print(allll())
+
+a = 212.22
+print(isinstance(a,int))
+
+# operators
+print(10 * 5)
+print(44 / 4)
+print(88 + 2)
+
+if 7 > 4:
+    print('correct')
+
+if 8 == 8 or 44 == 44:
+    print('two correct')
+
+
+"""
+list = ['asdf', 'cvbn', 'ertyu']
+print(list)
+print(len(list))
+print(list[2])
+print(list[-2])
+print(list[0:1])
+list.append('vbnm')
+list.insert(1, 'qwer')
+print(list)
+mist = ['gkkkk', 'rrrr', 'mmbnnmm']
+list.extend(mist)
+print(list)
+list.remove('rrrr')
+list.remove('vbnm')
+print(list)
+list.pop(3)
+print(list)
+del list[4]
+print(list)
+
+list.clear()
+print(list)
+"""
